f62b150c5da9a32e02b226bfda7642d0deeb2e06
arthursusilo4 <arthursusilo4@gmail.com>
2025-05-24 20:42:48 +0700

    Pembuatan Navbar dan Navitems

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Bricolage_Grotesque, Roboto_Slab } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar";
 
 const bricolage = Bricolage_Grotesque({
   variable: "--font-bricolage",
@@ -26,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${bricolage.variable} ${robotoSlab.variable} antialiased`}>
+        <Navbar />
         {children}
       </body>
     </html>
---
+++ components/navItems.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { cn } from "@/lib/utils";
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+const navItems = [
+     {label: 'Home', href: '/'},
+     {label: 'Buddies', href: '/buddies'},
+     {label: 'My Track', href: '/my-track'}
+]
+
+const NavItems = () => {
+     const pathname = usePathname();
+
+  return (
+     <nav className='flex items-center gap-4'>
+          {navItems.map(({label, href}) => (
+               <Link href={href} key={label} className={cn(pathname === href && 'text-primary font-bold')}>
+                    {label}
+               </Link>
+          ))}
+     </nav>
+  )
+}
+
+export default NavItems
