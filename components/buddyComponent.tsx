7c48f4b7bd7827afbbd24c5c79e5705a3a6f726c
arthursusilo4 <arthursusilo4@gmail.com>
2025-06-19 14:15:00 +0700

    update penyesuaian index.d.ts dan penambahan destrukturisasi parameter pada buddy component

--- components/buddyComponent.tsx
+++ components/buddyComponent.tsx
@@ -1,10 +1,17 @@
+import { getSubjectColor } from '@/lib/utils'
 import React from 'react'
 
-const BuddyComponent = () => {
+const BuddyComponent = ({buddyId, subject, topic, name, userName, userImage, style, voice}: BuddyComponentProps) => {
   return (
-    <div>
-      
-    </div>
+    <section className='flex flex-col h-[70vh]'>
+      <section className='flex gap-8 max-sm:flex-col'>
+          <div className='companion-section'>
+               <div className='companion-avatar' style={{ backgroundColor: getSubjectColor(subject) }}>
+
+               </div>
+          </div>
+      </section>
+    </section>
   )
 }
 
