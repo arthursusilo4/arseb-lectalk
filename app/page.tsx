d6b3010f0f5ad29099163b1ba2b2086760520944
arthursusilo4 <arthursusilo4@gmail.com>
2025-06-22 21:40:31 +0700

    update halaman utama untuk menampilkan popular buddies dan recent session dinamis

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,39 @@
-import React from 'react'
-import BuddyCard from '@/components/buddyCard'
-import BuddyList from '@/components/buddyList'
-import CallToAction from '@/components/callToAction'
-import { recentSessions } from '@/constants'
+import React from "react";
+import BuddyCard from "@/components/buddyCard";
+import BuddyList from "@/components/buddyList";
+import CallToAction from "@/components/callToAction";
+import { recentSessions } from "@/constants";
+import { getAllBuddies, getRecentSessions } from "@/lib/actions/buddy.actions";
+import { getSubjectColor } from "@/lib/utils";
 
+const Page = async () => {
+  const buddies = await getAllBuddies({ limit: 3 });
+  const recentSessionsBuddies = await getRecentSessions(10);
 
-const Page = () => {
   return (
     <main>
-      <h1 className=''>Buddy Terpopuler!</h1>
+      <h1 className="">Buddy Terpopuler!</h1>
 
-      <section className='home-section'>
-        <BuddyCard
-          ID='123'
-          name='Paulus Pemandu 1'
-          topic='Ahlinya Olahragawan'
-          subject='Olahraga'
-          duration={35}
-          color='#FF6B6B'
-        />
-
-        <BuddyCard
-          ID='124'
-          name='Paulus Pemandu 2'
-          topic='Ahlinya Olahragawan'
-          subject='Olahraga'
-          duration={15}
-          color='#E74C3C'
-        />
-
-        <BuddyCard
-          ID='125'
-          name='Paulus Pemandu 3'
-          topic='Ahlinya Olahragawan'
-          subject='Olahraga'
-          duration={55}
-          color='#FF3E4D'
-        />
+      <section className="home-section">
+        {buddies.map((buddy) => (
+          <BuddyCard
+            key={buddy.id}
+            {...buddy}
+            color={getSubjectColor(buddy.subject)}
+          />
+        ))}
       </section>
 
-      <section className='home-section'>
-        <CallToAction/>
+      <section className="home-section">
+        <CallToAction />
         <BuddyList
-          title='Sesi Selesai Terkini'
-          buddies={recentSessions}
-          classNames='w-2/3 max-lg:w-full'
+          title="Recently Completed Sessions"
+          buddies={recentSessionsBuddies}
+          classNames="w-2/3 max-lg:w-full"
         />
       </section>
     </main>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
