e82902de19fb3892da7ac8833980da24181dfbcf
arthursusilo4 <arthursusilo4@gmail.com>
2025-06-29 23:46:54 +0700

    update bug fix styling border search input

--- components/searchInput.tsx
+++ components/searchInput.tsx
@@ -34,14 +34,16 @@ const SearchInput = () => {
         }
       }
     }, 500);
+
+    return () => clearTimeout(delayDebounceFn);
   }, [searchQuery, router, searchParams, pathname]);
 
   return (
-    <div className="relative border border-black rounded-lg items-center flex gap-2 px-2 py-1 h-fit">
+    <div className="relative border border-black rounded-lg items-center flex gap-2 px-2 py-1 bg-white hover:border-[#d26805] transition-colors duration-200">
       <Image src="/icons/search.svg" alt="search" width={20} height={20} />
       <input
         placeholder="Search buddy here ..."
-        className="outline-none"
+        className="outline-none bg-transparent flex-1"
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
       />
