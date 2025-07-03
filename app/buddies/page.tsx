7085cbebed557e1eccc195d5f0004dc0e78d5086
arthursusilo4 <arthursusilo4@gmail.com>
2025-07-03 23:08:34 +0700

    update view import penggunaan function buddies id spesific pada buddies page

--- app/buddies/page.tsx
+++ app/buddies/page.tsx
@@ -1,7 +1,7 @@
 import BuddyCard from "@/components/buddyCard";
 import SearchInput from "@/components/searchInput";
 import SubjectFilter from "@/components/subjectFilter";
-import { getAllBuddies } from "@/lib/actions/buddy.actions";
+import { getUserBuddiesWithFilters } from "@/lib/actions/buddy.actions";
 import { getSubjectColor } from "@/lib/utils";
 
 const BuddiesLibrary = async ({ searchParams }: SearchParams) => {
@@ -9,14 +9,14 @@ const BuddiesLibrary = async ({ searchParams }: SearchParams) => {
   const subject = filters.subject ? filters.subject : "";
   const topic = filters.topic ? filters.topic : "";
 
-  const buddies = await getAllBuddies({ subject, topic });
+  const buddies = await getUserBuddiesWithFilters({ subject, topic });
 
   console.log(buddies);
 
   return (
     <main>
       <section className="flex justify-between gap-4 max-sm:flex-col">
-        <h1>Buddy Books</h1>
+        <h1>My Buddy Books</h1>
         <div className="flex gap-5">
           <SearchInput />
           <SubjectFilter />
@@ -24,13 +24,19 @@ const BuddiesLibrary = async ({ searchParams }: SearchParams) => {
       </section>
 
       <section className="buddies-grid">
-        {buddies.map((buddy) => (
-          <BuddyCard 
-          key={buddy.id} 
-          {...buddy} 
-          color={getSubjectColor(buddy.subject)}
-          />
-        ))}
+        {buddies.length > 0 ? (
+          buddies.map((buddy) => (
+            <BuddyCard 
+              key={buddy.id} 
+              {...buddy} 
+              color={getSubjectColor(buddy.subject)}
+            />
+          ))
+        ) : (
+          <div className="col-span-full text-center py-8">
+            <p className="text-gray-500">No buddies found. Create your first buddy!</p>
+          </div>
+        )}
       </section>
     </main>
   );
